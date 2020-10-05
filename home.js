a658a7cc8a87ba6c8e83d7df07748992853cfaa4
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-04 19:13:27 -0700

    If else conditionals and switch statements

--- home.js
+++ home.js
@@ -152,6 +152,39 @@ console.log(student.age);
 
 console.log(student.studentInfo());
 
+// conditionals, control flows (if else)
+// 18-35 is my rarget demographic 
+
+// && AND 
+// || OR 
+
+//const age = prompt('what is your age');
+
+// if( (age >= 18 ) && (age <= 35 ) ){
+//    status = 'target demo'; 
+//    console.log(status);
+// } else {
+//     status = 'not my audience';
+//     console.log(status);
+// }
+
+// Switch Statements 
+
+switch(5) {
+    case 0:
+        text = 'weekend';
+        break;
+    case 5:
+        text = 'weekend';
+        break;
+    case 6:
+        text = 'weekend';
+        break;
+    default: 
+        text = 'weekday';
+        break;
+}
+console.log(text);
 
 
 
