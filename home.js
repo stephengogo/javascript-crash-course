2423f7ebba43e908c37560e9b29cbfb4047299a5
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-04 17:07:46 -0700

    Manipulate the DOM

---
+++ home.js
@@ -0,0 +1,17 @@
+console.log('hello');
+console.log('hello');
+
+// alert('hello this is stephen');
+
+// Variables 
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+document.getElementById('someText').innerHTML = 'Hey There';
+
+var age = prompt('What is your age?');
+
+document.getElementById('someText').innerHTML = age;
