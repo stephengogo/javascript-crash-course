52ef5e613576ded12b6cc6b8de66ae5cd06ae495
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-04 17:28:06 -0700

    Data Types in Javascript

--- home.js
+++ home.js
@@ -62,5 +62,14 @@ while (num < 100) {
 
 for ( let num = 0; num < 100; num++){
     console.log(num);
-    
 }
+
+// Data types 
+let yourAge = 18; // numbner 
+let yourName = 'bob'// String
+
+let name = {first: 'Jane', last: 'Doe'} // object 
+let truth = false; // boolean 
+let groceries = ['apple', 'banana', 'oranges'] // array 
+let random; // undefined 
+let nothing = null; // value null
