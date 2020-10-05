6943cfbecdb607f8efca176eecc536dfe388e296
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-04 18:59:27 -0700

    Objects in Javascript

--- home.js
+++ home.js
@@ -129,4 +129,39 @@ for (let num = 0; num <= 10; num++) {
 }
 console.log(emptyArray);
 
+// objects in javascript 
+// dictionaries in python
+
+let student = {
+    first: 'Stephen', 
+    last: 'Qazi', 
+    age: 25, 
+    height: 170,
+    studentInfo: function() {
+        return this.first + '\n' + this.last + '\n' + this.age;
+    }
+};
+
+console.log(student.first);
+console.log(student.last);
+
+student.first = 'notStephen'; // change value 
+console.log(student.first);
+student.age++;
+console.log(student.age);
+
+console.log(student.studentInfo());
+
+
+
+
+
+
+
+
+
+
+
+
+
 
