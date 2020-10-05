b1b090110ee2503c35692c3942bc1f9655f6ca84
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-04 17:20:20 -0700

    Functions in Javascript

--- home.js
+++ home.js
@@ -8,10 +8,49 @@ var b = 'smoothie';
 console.log(b);
 
 var someNumber = 45;
-console.log(someNumber);
+//console.log(someNumber);
 
-document.getElementById('someText').innerHTML = 'Hey There';
+// Manipulate DOM with Javascript 
+// It's just a Fancy way of saying 
+// change HTML with Javascript
 
-var age = prompt('What is your age?');
+// document.getElementById('someText').innerHTML = 'Hey There';
+// var age = prompt('What is your age?');
+// document.getElementById('someText').innerHTML = age;
 
-document.getElementById('someText').innerHTML = age;
+
+// Numbers in Javascript EASY
+var num1 = 10;
+num1 = num1 + 1;
+console.log(num1);
+
+// Functions 
+//Create
+function fun() {
+    alert('this is a function')
+}
+// Call 
+//fun()
+
+// Lets create a function that takes in a name 
+// and says hello followed by your name 
+
+// For example 
+// Name: "Stephen"
+// Return "Hello Stephen"
+
+function  greeting(name) {
+    
+    var result = 'Hello ' + name;
+    console.log(result);
+}
+var name = prompt('what is your name?');
+//greeting()
+
+
+
+function sumNumbers(num1, num2){
+    var result = num1 + num2;
+    console.log(result); 
+}
+sumNumbers('10', 10) // prints 1010
